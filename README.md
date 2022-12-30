5853a912e662d1036b3193e9c9835411f4d2cf7e
MalfinK <muhammadalfin1235678@gmail.com>
2022-12-30 17:05:23 +0700

    Menambahkan file baru

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Project-Basdat-Checkout
+
+- Muhammad Alfin Khaerudin - 1313621003
+- Muhammad Izzat Azizan - 13136210
+- Muhammad Rafly Surya Nirwana - 13136210
---
+++ script.js
@@ -0,0 +1,28 @@
+// swipper
+var swiper = new Swiper(".mySwiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true,
+    },
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
