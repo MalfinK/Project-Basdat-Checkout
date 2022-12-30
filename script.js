7bdd3daedfc04cd91cd659c14c75bc69d6ef52a1
MalfinK <muhammadalfin1235678@gmail.com>
2022-12-30 18:55:19 +0700

    Edit file all

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 // swipper
 var swiper = new Swiper(".mySwiper", {
-    effect: "coverflow",
-    grabCursor: true,
-    centeredSlides: true,
-    slidesPerView: "auto",
-    coverflowEffect: {
-        rotate: 50,
-        stretch: 0,
-        depth: 100,
-        modifier: 1,
-        slideShadows: true,
-    },
+    // effect: "coverflow",
+    // grabCursor: true,
+    // centeredSlides: true,
+    // slidesPerView: "auto",
+    // coverflowEffect: {
+    //     rotate: 50,
+    //     stretch: 0,
+    //     depth: 100,
+    //     modifier: 1,
+    //     slideShadows: true,
+    // },
     spaceBetween: 30,
     centeredSlides: true,
     autoplay: {
@@ -20,9 +20,40 @@ var swiper = new Swiper(".mySwiper", {
     pagination: {
         el: ".swiper-pagination",
         clickable: true,
+        dynamicBullets: true,
     },
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
+    // navigation: {
+    //     nextEl: ".swiper-button-next",
+    //     prevEl: ".swiper-button-prev",
+    // },
 });
+
+// Path: script.js
+
+
+const navItem = document.querySelectorAll('.nav-item');
+
+function linkAction() {
+    /*Active link*/
+    navItem.forEach((n) => n.classList.remove("active")); // arrow function
+    this.classList.add("active"); // this ini berguna untuk men select semua yang ada di function
+}
+navItem.forEach(el1 => {
+    el1.addEventListener('click', linkAction)
+
+    el1.addEventListener('mouseenter', function () {
+        el1.style.fontFamily = 'Algerian';
+        el1.style.backgroundColor = 'yellow';
+    })
+    
+    el1.addEventListener('mouseleave', function () {
+        el1.style.fontFamily = 'normal';
+        el1.style.backgroundColor = '#e3f2fd';
+    })
+});
+
+
+const navBrand = document.querySelector('.navbar-brand');
+navBrand.addEventListener('click', function () {
+    navItem.forEach((n) => n.classList.remove("active")); // arrow function
+})
